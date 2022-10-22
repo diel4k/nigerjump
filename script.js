5ca92d3152dfa0e705844e0bd3a22ef1255108da
daniel4ik <112887790+diel4k@users.noreply.github.com>
2022-10-23 00:14:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+document.addEventListener("keydown",function(event){
+    jump()
+})
+function jump(){
+if (dino.classList != 'jump')
+dino.classList.add("jump");
+setTimeout(function(){
+dino.classList.remove("jump");
+}, 600)
+}
+let isAlive = setInterval(function(){
+let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+let chaykaLeft = parseInt(window.getComputedStyle(chayka).getPropertyValue("left"));
+if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+alert("game over")
+}
+if (chaykaLeft < 50 && chaykaLeft > 0 && dinoTop >= 140){
+alert("game over")
+}
+// if (let srow = 0, i < 100, i++,actusLeft < 0){
+// let score = document.getElementById("s");
+// score.innerHTML = srow;
+// }
+}, 10)
